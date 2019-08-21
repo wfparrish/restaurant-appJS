7f77189562dc58bbe13b8253bf45e663ce3a2422
williamparrish <williparrish@gmail.com>
2019-08-20 21:57:14 -0600

    implemented projectBox for holding projectDisplays

--- products.js
+++ products.js
@@ -1,6 +1,16 @@
 class Products {
   constructor() {
     this.viewBuilder = [];
+    this.myArrProducts = [];
+  }
+
+  passDataToUserLog(id) {
+    this.selectedProdDisp = document.getElementById(id);
+    //this.selectedProdDisp = burger1;                //Will create a array filled with burgers
+    console.log(this.selectedProdDisp);
+    this.myArrProducts.push(this.selectedProdDisp);
+    this.viewBuilder = this.myArrProducts;
+    console.log(productBox.viewBuilder);
   }
 }
 
@@ -10,6 +20,14 @@ class ProductDisplays {
     this.menuItem = menuItem;
   }
 }
+
+let productBox = new Products();
+let productDisp1 = new ProductDisplays(1, burger1);
+let productDisp2 = new ProductDisplays(2, fries1);
+let productDisp3 = new ProductDisplays(3, milkshake1);
+let productDisp4 = new ProductDisplays(4, chix0);
+productBox.passDataToUserLog("addMeBtn1");
+console.log(productBox.viewBuilder);
 
 //I should be able to use the Order class to start to create ProductDisplay methods. We should be using parts from the order.js file
 
