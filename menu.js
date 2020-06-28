2be0b03a0111914d13cea4afe47ec4fb1e5d5e7b
williamparrish <williparrish@gmail.com>
2020-06-28 12:28:52 -0600

    modified Burger, made it shorter

--- menu.js
+++ menu.js
@@ -3,15 +3,6 @@ class Burger {
     this.name = "Burger";
     this.price = 3.89;
     this.temperature = "";
-    this.cheese = false;
-    this.tomato = false;
-    this.lettuce = false;
-    this.onion = false;
-    this.ketchup = false;
-    this.mustard = false;
-    this.mayo = false;
-    this.bacon = false;
-    this.pickle = false;
   }
 }
 
